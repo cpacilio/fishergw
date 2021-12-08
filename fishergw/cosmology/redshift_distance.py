0b9690f9e1030a4c6d5190733b9937a6fe2784c0
costantino pacilio <costantinopacilio7@hotmail.it>
2021-12-08 17:13:12 +0100

    redshift_distance update with better solver at low redshifts

--- fishergw/cosmology/redshift_distance.py
+++ fishergw/cosmology/redshift_distance.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy.integrate import simps
-from scipy.optimize import root_scalar
+from scipy.optimize import root_scalar, fsolve
 
 from ..constants import speed_of_light, omega_matter, omega_lamda, H0
 cc = speed_of_light*1e-3 # Km/s
@@ -16,7 +16,7 @@ def distance_from_redshift(z):
     :rtype: float
     """
     integrand = lambda x: 1/np.sqrt(omega_matter*(1+x)**3+omega_lamda)
-    dz = 1e-3
+    dz = 1e-5
     X = np.arange(0,z+dz,dz)
     Y = integrand(X)
     d_L = cc/H0*(1+z)*simps(Y,X)
@@ -28,16 +28,12 @@ def redshift_from_distance(d_L):
 
     Uses the Planck 2018 cosmological parameters from Tab. 1 in https://arxiv.org/abs/1807.06209.
 
-    :param float d_L: Luminosity distance [Mpc]. Must be less than 231518.
+    :param float d_L: Luminosity distance [Mpc]. Must be no less than 1.
     
     :rtype: float
-
-    .. note::
-
-       A solution for z is searched in the interval [0,20]. This restricts the allowed values of d_L in [0,231518] Mpc.
     """
-    if d_L>231518:
-        raise ValueError('d_L must be less than 231518 Mpc!')
+    if d_L<1:
+        raise ValueError('d_L must be no less than 1 Mpc!')
     f = lambda x: d_L - distance_from_redshift(x)
-    z = root_scalar(f,method='bisect',bracket=[0,20])
-    return z.root
+    z = fsolve(f,10).item()
+    return z
